d0433a4ff95814122c58d27abc8ed1b903653601
HoTienQuoc <hotienquoc0429@gmail.com>
2025-07-14 21:21:28 +0700

    Refactor product API endpoints to use MongoDB for data retrieval and creation, adding error handling for database operations.

--- backend/app.js
+++ backend/app.js
@@ -8,23 +8,34 @@ const mongoose = require('mongoose');
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 
+
 require("dotenv/config");
 
 const api = process.env.API_URL;
 
-app.get(`${api}/products`, (req,res)=>{
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url'
+app.get(`${api}/products`, async (req,res)=>{
+    const productList = await Product.find();
+    if(!productList){
+        res.status(500).json({success:false})
     }
-    res.send(product);
+    res.send(productList);
 })
 
 app.post(`${api}/products`, (req,res)=>{
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    });
+    product.save().then((createdProduct=>{
+        res.status(201).json(createdProduct)
+    })).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    });
+    res.send(product);
 })
 
 mongoose.connect(process.env.CONNECTION_STRING,{
