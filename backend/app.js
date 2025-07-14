762f5f359c38f67a5a3b4ae0e9e6752d5de58abc
HoTienQuoc <hotienquoc0429@gmail.com>
2025-07-14 15:47:39 +0700

    Add product API endpoints and JSON middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send("hello Api");
+//Middleware 
+app.use(express.json())
+
+require("dotenv/config");
+
+const api = process.env.API_URL;
+
+app.get(`${api}/products`, (req,res)=>{
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url'
+    }
+    res.send(product);
+})
+
+app.post(`${api}/products`, (req,res)=>{
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 })
 
 app.listen(3000, ()=>{
+    console.log(api);
     console.log("Server is running http://localhost:3000");
 })
