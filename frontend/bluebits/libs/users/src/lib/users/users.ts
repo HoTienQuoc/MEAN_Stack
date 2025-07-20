29edbd23e82405b517dc7196b37e25e29d8128b4
HoTienQuoc <hotienquoc0429@gmail.com>
2025-07-20 16:01:08 +0700

    Update tsconfig.base.json to include new module paths for products and users, enhancing module resolution for the project.

---
+++ frontend/bluebits/libs/users/src/lib/users/users.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-users',
+  imports: [CommonModule],
+  templateUrl: './users.html',
+  styleUrl: './users.scss',
+})
+export class Users {}
